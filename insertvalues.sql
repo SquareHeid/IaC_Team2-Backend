816589505323a5de56cbfae17cdba113993884df
SquareHeid <99138465+SquareHeid@users.noreply.github.com>
2024-10-16 11:50:23 +0100

    Create insertvalues.sql

---
+++ insertvalues.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb1 (firstname, rollnumber) VALUES ('Adam',1);
+INSERT INTO tb1 (firstname, rollnumber) VALUES ('Andrew',2);
+INSERT INTO tb1 (firstname, rollnumber) VALUES ('Harry',3);
+INSERT INTO tb1 (firstname, rollnumber) VALUES ('Tom',4);
+INSERT INTO tb1 (firstname, rollnumber) VALUES ('Dick',5);
