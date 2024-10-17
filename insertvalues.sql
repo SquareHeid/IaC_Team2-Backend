7165dbbfdac02d6e85862909167e5d66a16b4250
SquareHeid <99138465+SquareHeid@users.noreply.github.com>
2024-10-17 14:38:38 +0100

    Update insertvalues.sql

--- insertvalues.sql
+++ insertvalues.sql
@@ -2,3 +2,4 @@ INSERT INTO tb1 (firstname, rollnumber) VALUES ('Meraj',1);
 INSERT INTO tb1 (firstname, rollnumber) VALUES ('Adam',2);
 INSERT INTO tb1 (firstname, rollnumber) VALUES ('Shiju',3);
 INSERT INTO tb1 (firstname, rollnumber) VALUES ('Sagar',4);
+INSERT INTO tb1 (firstname, rollnumber) VALUES ('Ramesh',99);
